734cb761c99e9117bb4ea2023dad618652515d2d
PChabowski <117765708+PChabowski@users.noreply.github.com>
2023-09-17 19:16:37 +0200

    TM 13

--- menu.js
+++ menu.js
@@ -6,19 +6,19 @@ import openMenu from "./modules/openMenu.js";
 let name = person.name;
 document.getElementById("demo").innerHTML = name;
 
-const header = ['strona-glowna', 'o-mnie', 'projekty', 'kontakt'];
 
 window.onscroll = function() {scrollFunction()};
 window.onload = function() {
 	const btnGoTop = document.getElementById("goTop");
 	btnGoTop.onclick = goTop;
-
+	
 	const btnOpenMenu = document.getElementById("open-menu");
 	btnOpenMenu.onclick = openMenu;
-
+	
+	const title = ['strona-glowna', 'o-mnie', 'projekty', 'kontakt'];
 	const topnav = document.getElementById("Topnav");
 	const linkMenu = topnav.getElementsByTagName("a");
-	for(let i=0; header.length; i++) {
-		linkMenu[i].onclick = onePage(header[i], this);
+	for(let i=0; linkMenu.length; i++) {
+		linkMenu[i].onclick = onePage(title[i], this);
 	}
 };
