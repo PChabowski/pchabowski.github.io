93a17d6de3a1b6f8ef34c456b3e2da54d6ee7204
PChabowski <117765708+PChabowski@users.noreply.github.com>
2023-09-17 18:29:01 +0200

    test modułu 3

--- menu.js
+++ menu.js
@@ -1,10 +1,15 @@
 import person from "./modules/person.js";
 import scrollFunction, { goTop } from "./modules/scrollFunction.js";
+import onePage from "./modules/onePage.js";
+import openMenu from "./modules/openMenu.js";
+
 let name = person.name;
 document.getElementById("demo").innerHTML = name;
 
 window.onscroll = function() {scrollFunction()};
 window.onload = function() {
 	const btnGoTop = document.getElementById("goTop");
+	const btnOpenMenu = document.getElementById("open-menu");
 	btnGoTop.onclick = goTop;
+	btnOpenMenu.onclick = openMenu;
 };
