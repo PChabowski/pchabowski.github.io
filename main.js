5d7b05852e7fe54b87759218e034398eb1ba0c50
PChabowski <p.chabowski1@gmail.com>
2023-11-04 20:48:30 +0100

    new layout in html and css

--- main.js
+++ main.js
@@ -3,6 +3,23 @@ import { initSFunAndGoTop } from "./modules/scrollFunction.js";
 import { initOpenMenu } from "./modules/openMenu.js";
 import { initOnePage } from "./modules/onePage.js";
 
+fetch('date/person.json')
+    .then(response => response.json())
+    .then(data => {
+        const persons = data.person;
+        const personIdToFind = 1;
+
+        const foundPerson = persons.find(person => person.id === personIdToFind);
+
+        if (foundPerson) {
+            const personConteiner = document.querySelector('.header');
+            personConteiner.innerHTML = `<h1>${foundPerson.name} ${foundPerson.surname}</h1><p>${foundPerson.profession}</p>`;
+
+        } else {
+            console.log('Nie znaleziono takiej osoby');
+        }
+    });
+
 window.onload = function () {
 	document.getElementById("demo").innerHTML = person.name;
 	initSFunAndGoTop();
